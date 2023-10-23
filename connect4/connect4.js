0744c608ee4478e76e29ecaad4bcc4ff8138f93f
ikgron <ikgronemeier@gmail.com>
2023-10-23 00:43:05 -0700

    connect4 fix

--- connect4/connect4.js
+++ connect4/connect4.js
@@ -1,117 +1,117 @@
-(function() {
+(function () {
 
-	var ConnectFour = function() {
+	var ConnectFour = function () {
 
 		gameBoard = {};
 		currentPlayer = 'red';
 		numRows = 6;
 		numCols = 7;
 		numTurns = 0;
-		
-		_init = function() {
-			
+
+		_init = function () {
+
 			var columns;
-			
+
 			columns = document.querySelectorAll('.column');
-			
-			Array.prototype.forEach.call(columns, function(col) {
-				col.addEventListener('click', function() {
+
+			Array.prototype.forEach.call(columns, function (col) {
+				col.addEventListener('click', function () {
 					markNextFree(col.getAttribute('data-x'));
 				});
 			});
-			
-			for(var x = 0; x <= numRows; x++) {
-			
+
+			for (var x = 0; x < numCols; x++) { // Update loop limits for columns
+
 				gameBoard[x] = {};
-				
-				for(var y = 0; y <= numCols; y++) {
+
+				for (var y = 0; y < numRows; y++) { // Update loop limits for rows
 					gameBoard[x][y] = 'free';
 				}
 			}
-			
 		};
-		
-		var markNextFree = function(x) {
+
+
+		var markNextFree = function (x) {
 
 			var nextY;
-			
+
 			nextY = false;
-			
-			for(var y = 0; y < numRows; y++) {
-				if(gameBoard[x][y] === 'free') {
+
+			for (var y = 0; y < numRows; y++) {
+				if (gameBoard[x][y] === 'free') {
 					nextY = y;
 					break;
 				}
 			}
-			
-			if(nextY === false) {
+
+			if (nextY === false) {
 				alert('No free spaces in this column. Try another.');
 				return false;
 			}
-			
+
 			gameBoard[x][nextY] = currentPlayer;
-			
-			document.querySelector('#column-'+x+' .row-'+nextY+' circle').setAttribute(
-					'class', currentPlayer
+
+			document.querySelector('#column-' + x + ' .row-' + nextY + ' circle').setAttribute(
+				'class', currentPlayer
 			);
-			
-			if(isWinner(parseInt(x), nextY)) {
-				alert(currentPlayer+' wins!');
+
+			if (isWinner(parseInt(x), nextY)) {
+				alert(currentPlayer + ' wins!');
 				clearBoard();
 				return true;
 			}
 
 			numTurns = numTurns + 1;
 
-			if(numTurns >= numRows * numCols) {
+			if (numTurns >= numRows * numCols) {
 				alert('It\'s a tie!');
 				clearBoard();
-				return true;				
+				return true;
 			}
 
 			currentPlayer = currentPlayer === 'red' ? 'yellow' : 'red';
 
 		};
-		
-		var clearBoard = function() {
-			
-			Array.prototype.forEach.call(document.querySelectorAll('circle'), function(piece) {
+
+		var clearBoard = function () {
+
+			Array.prototype.forEach.call(document.querySelectorAll('circle'), function (piece) {
 				piece.setAttribute('class', 'free');
 			});
-			
+
 			gameBoard = {};
 
-			for(var x = 0; x <= numRows; x++) {
-			
+			for (var x = 0; x <= numRows; x++) {
+
 				gameBoard[x] = {};
-				
-				for(var y = 0; y <= numCols; y++) {
+
+				for (var y = 0; y <= numCols; y++) {
 					gameBoard[x][y] = 'free';
 				}
-        
-          console.log(gameBoard);
+
+				console.log(gameBoard);
 			}
 
 			numTurns = 0;
-			
+
 			return gameBoard;
 
 		};
 
-		var isWinner = function(currentX, currentY) {
-			return checkDirection(currentX, currentY, 'vertical') || 
-				checkDirection(currentX, currentY, 'diagonal') || 
+		var isWinner = function (currentX, currentY) {
+			return checkDirection(currentX, currentY, 'vertical') ||
+				checkDirection(currentX, currentY, 'diagonal') ||
 				checkDirection(currentX, currentY, 'horizontal');
 		};
-		
-		var isBounds = function(x, y) {
+
+		var isBounds = function (x, y) {
 			return (gameBoard.hasOwnProperty(x) && typeof gameBoard[x][y] !== 'undefined');
 		};
 
-		var checkDirection = function(currentX, currentY, direction) {
-		
+		var checkDirection = function (currentX, currentY, direction) {
+
 			var chainLength, directions;
-			
+
 			directions = {
 				horizontal: [
 					[0, -1], [0, 1]
@@ -123,31 +123,31 @@
 					[-1, -1], [1, 1], [-1, 1], [1, -1]
 				]
 			};
-			
+
 			chainLength = 1;
-			
-			directions[direction].forEach(function(coords) {
-				
+
+			directions[direction].forEach(function (coords) {
+
 				var i = 1;
 
-				while( isBounds(currentX + (coords[0] * i), currentY + (coords[1] * i)) && 
+				while (isBounds(currentX + (coords[0] * i), currentY + (coords[1] * i)) &&
 					(gameBoard[currentX + (coords[0] * i)][currentY + (coords[1] * i)] === currentPlayer)
 				) {
-					chainLength = chainLength + 1; 
-					i = i + 1; 
+					chainLength = chainLength + 1;
+					i = i + 1;
 				};
-				
+
 			});
-			
+
 			return (chainLength >= 4);
-			
+
 		};
-		
+
 		_init();
-		
+
 	};
 
 	ConnectFour();
-  
-	
+
+
 })();
