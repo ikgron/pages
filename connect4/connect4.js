07e1c48d3f8b40d324c5b11c86a2dac7cb9094ea
ikgron <ikgronemeier@gmail.com>
2023-11-07 14:08:40 -0800

    added win note, fixed buttons

--- connect4/connect4.js
+++ connect4/connect4.js
@@ -1,17 +1,19 @@
 (function () {
 	var ConnectFour = function () {
-		gameBoard = {};
-		currentPlayer = 'red';
-		numRows = 6;
-		numCols = 7;
-		numTurns = 0;
+		var gameBoard = {};
+		var currentPlayer = 'red';
+		var numRows = 6;
+		var numCols = 7;
+		var numTurns = 0;
+		var gameInProgress = false; // Add a flag to track whether a game is in progress
 
-		_init = function () {
+		var _init = function () {
 			var columns;
 			columns = document.querySelectorAll('.column');
 
 			Array.prototype.forEach.call(columns, function (col) {
 				col.addEventListener('click', function () {
+					if (gameInProgress) return; // Don't allow clicks while a move is being processed
 					markNextFree(col.getAttribute('data-x'));
 				});
 			});
@@ -30,17 +32,26 @@
 		var playRedTurn = function () {
 			// Red player's turn
 			currentPlayer = 'red';
+			gameInProgress = false; // Enable input for the current player
 		};
 
 		var playYellowTurn = function () {
 			// Yellow player's turn
 			currentPlayer = 'yellow';
+			gameInProgress = true; // Disable input while AI move is being processed
 
 			setTimeout(function () {
 				var randomColumn = Math.floor(Math.random() * numCols);
 				markNextFree(randomColumn);
 			}, 600);
 		};
+
+		function displayWinMessage(player) {
+			var winMessage = document.getElementById("winMessage");
+			var capitalizedPlayer = player.charAt(0).toUpperCase() + player.slice(1);
+			winMessage.textContent = capitalizedPlayer + " wins!";
+			winMessage.style.fontSize = "2rem";
+		}
 
 
 		var markNextFree = function (x) {
@@ -54,6 +65,7 @@
 
 			if (nextY === false) {
 				// alert('No free spaces in this column. Try another.');
+				gameInProgress = false; // Allow input again
 				return false;
 			}
 
@@ -63,8 +75,7 @@
 			);
 
 			if (isWinner(parseInt(x), nextY)) {
-				alert(currentPlayer + ' wins!');
-				clearBoard();
+				displayWinMessage(currentPlayer);
 				return true;
 			}
 
@@ -85,7 +96,6 @@
 		};
 
 		var clearBoard = function () {
-
 			Array.prototype.forEach.call(document.querySelectorAll('circle'), function (piece) {
 				piece.setAttribute('class', 'free');
 			});
@@ -93,23 +103,18 @@
 			gameBoard = {};
 
 			for (var x = 0; x <= numRows; x++) {
-
 				gameBoard[x] = {};
 
 				for (var y = 0; y <= numCols; y++) {
 					gameBoard[x][y] = 'free';
 				}
-
-				console.log(gameBoard);
 			}
 
 			numTurns = 0;
-
-			return gameBoard;
-
+			gameInProgress = false; // Reset the game in progress flag
 		};
 
-		this.clearBoard = clearBoard
+		this.clearBoard = clearBoard;
 
 		var isWinner = function (currentX, currentY) {
 			return checkDirection(currentX, currentY, 'vertical') ||
@@ -122,7 +127,6 @@
 		};
 
 		var checkDirection = function (currentX, currentY, direction) {
-
 			var chainLength, directions;
 
 			directions = {
@@ -140,27 +144,20 @@
 			chainLength = 1;
 
 			directions[direction].forEach(function (coords) {
-
 				var i = 1;
-
 				while (isBounds(currentX + (coords[0] * i), currentY + (coords[1] * i)) &&
 					(gameBoard[currentX + (coords[0] * i)][currentY + (coords[1] * i)] === currentPlayer)
 				) {
 					chainLength = chainLength + 1;
 					i = i + 1;
-				};
-
+				}
 			});
 
 			return (chainLength >= 4);
-
 		};
 
 		_init();
-
 	};
 
 	ConnectFour();
-
-
 })();
